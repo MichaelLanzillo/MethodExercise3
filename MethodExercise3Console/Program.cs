15d46d625b0620da4156e9e83d82e2bbbc5ce5d9
Michael Lanzillo <lanzillo.michael1@gmail.com>
2020-04-08 12:14:41 -0500

    Started exercise 3 for Method Practice

---
+++ MethodExercise3Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MethodExercise3Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
